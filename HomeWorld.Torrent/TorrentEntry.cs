2c0aa51a5ac59439ada634749a8f60140e697853
Uladzimir Harabtsou <l0nley@users.noreply.github.com>
2018-11-22 11:22:00 +0300

    changes

--- HomeWorld.Torrent/TorrentEntry.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace HomeWorld.Torrent
-{
-    public class TorrentEntry
-    {
-        public TorrentMetadata Metadata { get; internal set; }
-        public string BasePath { get; internal set; }
-    }
-}
